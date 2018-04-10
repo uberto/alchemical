d974b780b0d7cc268de9edb48cfaa2f0e20b9e2e
Uberto Barbini <uberto.gama@gmail.com>
2018-04-10 21:16:20 +0100

    Added exercise rules

--- exerciseRisk/src/com/gamasoft/Main.java
+++ exerciseRisk/src/com/gamasoft/Main.java
@@ -9,12 +9,18 @@ import com.gamasoft.report.Report;
 import com.gamasoft.report.RiskData;
 import com.gamasoft.risk.RiskCalculator;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
 public class Main {
 
     public static void main(String[] args){
 
         ConnectionPool.getInstance().open("dbConn");
         Printer printer = new Printer("mainPrinter", System.out);
+//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//        Printer printer = new Printer("mainPrinter", byteArrayOutputStream);
 
         produceReport("Bud Fox", "LDN", printer);
         produceReport("Gordon Gekko", "NY", printer);
@@ -33,6 +39,7 @@ public class Main {
         Report rep = new Report(printer, riskData);
 
         printer.printReport(rep);
+
     }
 }
 
--- exerciseRisk/src/com/gamasoft/report/Printer.java
+++ exerciseRisk/src/com/gamasoft/report/Printer.java
@@ -2,6 +2,7 @@ package com.gamasoft.report;
 
 import com.gamasoft.commons.Utils;
 
+import java.io.OutputStream;
 import java.io.PrintStream;
 
 public class Printer {
@@ -9,8 +10,8 @@ public class Printer {
     public final String printerLocation;
     private PrintStream outStream;
 
-    public Printer(String printerLocation, PrintStream outStream) {
-        this.outStream = outStream;
+    public Printer(String printerLocation, OutputStream outStream) {
+        this.outStream = new PrintStream(outStream);
         Utils.notBlank(printerLocation, "printerLocation");
         this.printerLocation = printerLocation;
     }
